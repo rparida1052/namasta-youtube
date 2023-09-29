bd921f2c64e0f973e7531b117ba0b4d3d9789579
Rahul Parida <rparida1052@gmail.com>
2023-09-29 18:59:09 +0530

    icon:Updated

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux"
 import { toggleMenu } from "../../utils/appSlice";
+import { YOUTUBE_LOGO,HAMBURGER ,USER_AVATAR,SEARCH_ICON} from "../utils/constants";
 
 
 const Head = () => {
@@ -10,18 +11,18 @@ const Head = () => {
   return (
     <div className="grid grid-flow-col p-2  shadow-sm ">
       <div className="flex flex-row gap-3 col-span-1">
-        <img src="src\assets\icons8-hamburger-96.png" alt="hamburger icon" className="h-8 cursor-pointer"  onClick={()=>toggleSideMenu()}/>
-        <img src="src\assets\YouTube-Logo-Vector.png" alt="Youtube Logo" className="h-8 cursor-pointer"  />
+        <img src={HAMBURGER} alt="hamburger icon" className="h-8 cursor-pointer"  onClick={()=>toggleSideMenu()}/>
+        <img src={YOUTUBE_LOGO} alt="Youtube Logo" className="h-8 cursor-pointer"  />
       </div>
 
       <div className="col-span-10  flex justify-center items-center ">
         <input type="text" className="w-1/3 h-[40px] border-2 border-gray-500 rounded-l-full  p-4 font-bold text-xl"/>
         <button className="rounded-r-full  border-gray-500 border-2 p-0.5">
-          <img src="src\assets\icons8-search-50.png" alt="searchBar" className="h-8 place-content-center" />
+          <img src={SEARCH_ICON} alt="searchBar" className="h-8 place-content-center" />
         </button>
       </div>
       <div className="col-span-2">
-        <img src="src\assets\icons8-user-52.png" alt="user_logo" className="h-8" />
+        <img src={USER_AVATAR} alt="user_logo" className="h-8" />
       </div>
     </div>
   )
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,11 @@
 
 const GOOGLE_API_KEY = "AIzaSyBxc3Kq4ta6KmN9UUbPIcffQR3W-z56ek0";
  export  const  YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${GOOGLE_API_KEY}`;
+
+ export const YOUTUBE_LOGO = "https://res.cloudinary.com/dlhjzfcao/image/upload/v1695993710/namasta-youtube/YouTube-Logo-Vector_mnh8eb.png"
+
+ export const HAMBURGER = "https://res.cloudinary.com/dlhjzfcao/image/upload/v1695993710/namasta-youtube/icons8-hamburger-96_mjunot.png";
+
+ export const USER_AVATAR = "https://res.cloudinary.com/dlhjzfcao/image/upload/v1695993710/namasta-youtube/icons8-user-52_gihdmv.png";
+
+ export const SEARCH_ICON = "https://res.cloudinary.com/dlhjzfcao/image/upload/v1695993710/namasta-youtube/icons8-search-50_ets51o.png";
